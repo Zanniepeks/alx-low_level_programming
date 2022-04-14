506c665474038c4ed0d078d31b9501a01151bbdc
Zanniepeks <zainaboriyomi07@gmail.com>
2022-04-13 17:23:22 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int a = 0, b = 1, next = 0;
 	int sum = 0;
-
+	
 	while (next < 4000000)
 	{
 		next = a + b;
@@ -21,3 +21,4 @@ int main(void)
 	printf("%i\n", sum);
 	return (0);
 }
+
