533102043687efba5e6bb9b1079e6cf6244c24fd
Zanniepeks <zainaboriyomi07@gmail.com>
2022-04-23 23:13:14 +0100

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *  _strcmp - function compares two strings
+ *  @s1: pointer to a string
+ *  @s2: pointer to a string
+ *  Return: an integer.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, result = 0;
+
+	int a, b;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			a = s1[i];
+			b = s2[i];
+			result = a - b;
+			break;
+		}
+		++i;
+	}
+	return (result);
+}
